3ad5d617249ee310b3e01d257811d326f4b29c99
shadyabhi <abhijeet.1989@gmail.com>
2012-03-22 16:32:04 +0530

    Removed windows newline & indentation. Added README

---
+++ README.md
@@ -0,0 +1,22 @@
+# About the script
+
+Show ratings when you open actor's page in IMDb
+
+# Motivation
+
+If you are a movie freak, a day won't pass when you won't open an actor's page and find more movies to watch. The actor's page has the list of all the movies in which he acted but for some reason IMDb doesn't show the movie ratings. I feet to lazy to open each links and watch the rating. 
+
+ So, I wrote this extension to ease up the process. The ratings of movies are shown beside the names of the movies and after installing there will not be any need to open movie's link in new tab just to watch the rating.
+
+# Demonstration
+
+Screenshot: 
+
+![IMDb Ratings Viewer](http://i.imgur.com/Lks0R.jpg)
+
+Youtube Video: http://www.youtube.com/watch?v=LUz4DJGULtk
+
+# Installing Extension
+
+Chrome Users: https://chrome.google.com/webstore/detail/daaoegihbflfijkdhalidlmochdbobgb
+Firefox Users:  https://addons.mozilla.org/en-US/firefox/addon/imdb-1/
--- imdb.js
+++ imdb.js
@@ -6,15 +6,11 @@ Email: abhijeet.1989@gmail.com
 function addRating(url, element)
 {
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function()
-    {   
-        if ( request.readyState == 4 ) 
-        {   
-            callback_addrating( request.responseText, element, request.status );    
-        }   
-    };  
+    request.onreadystatechange = function(){
+        if ( request.readyState == 4 )  callback_addrating( request.responseText, element, request.status );
+    };
     request.open( "GET", url, true );
-    request.send()
+    request.send();
 }
 
 function getMovieElements(){
@@ -22,12 +18,11 @@ function getMovieElements(){
     var ele_even = document.getElementsByClassName("filmo-row even");
     var ele_odd = document.getElementsByClassName("filmo-row odd");
 
-    //Reoder the elements as right now, Nodelist for odd and even are separated. 
+    //Reoder the elements as right now, Nodelist for odd and even are separated.
     var all_elements = new Array(ele_even.length + ele_odd.length);
     var index = 0;
-    
     var max = ele_odd.length > ele_even.length?ele_odd:ele_even;
-    
+
     for (i=0; i<max.length; i++){
         if (i < ele_odd.length) all_elements[index++] = ele_odd[i];
         if (i < ele_even.length) all_elements[index++] = ele_even[i];
@@ -41,12 +36,12 @@ function callback_addrating(serverResponse, element, status){
     var rating = null;
     var match_rating = serverResponse.match(pattern);
     if (match_rating != null) rating = match_rating[1];
-    
+
     if (rating != null){
         var container = document.createElement("span");
         var rating_container = document.createElement("span");  //Did this crap to make brackets black
         rating_container.appendChild(document.createTextNode(rating));
-        rating_container.className = "in_production";   //Give it a red color to mantain consistency 
+        rating_container.className = "in_production";   //Give it a red color to mantain consistency
         container.appendChild(document.createTextNode(" ("));
         container.appendChild(rating_container);
         container.appendChild(document.createTextNode(")"));
@@ -64,7 +59,7 @@ function main(){
             addRating(movie_link, ele_moviename.childNodes[5]);
         }
     }
-    catch (err) {}; 
+    catch (err) {}; //Catches Exception when KnownFor is not present for certains Actors.
     all_elements = getMovieElements();
     for (i=0; i<all_elements.length; i++){
         var movie_link = all_elements[i].childNodes[3].childNodes[1].href;
